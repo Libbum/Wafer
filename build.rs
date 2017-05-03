ba3097e2211f563076b35233c5143209db7d6f46
Tim DuBois <timothy.dubois@chalmers.se>
2017-05-03 16:45:09 +0200

    Header with gitsha

---
+++ build.rs
@@ -0,0 +1,14 @@
+extern crate vergen;
+
+use vergen::vergen;
+
+fn main() {
+    let mut flags = vergen::OutputFns::all();
+    flags.toggle(vergen::COMMIT_DATE);
+    flags.toggle(vergen::NOW);
+    flags.toggle(vergen::SEMVER);
+    flags.toggle(vergen::SHORT_NOW);
+    flags.toggle(vergen::SHORT_SHA);
+    flags.toggle(vergen::TARGET);
+    assert!(vergen(flags).is_ok());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+include!(concat!(env!("OUT_DIR"), "/version.rs"));
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("                    ___");
+    println!("   __      ____ _  / __\\__ _ __");
+    println!("   \\ \\ /\\ / / _` |/ / / _ \\ '__|");
+    println!("    \\ V  V / (_| / _\\|  __/ |");
+    println!("     \\_/\\_/ \\__,/ /   \\___|_|    Current build SHA1: {}",
+             sha());
+    println!("              \\__/");
+    println!("");
 }
