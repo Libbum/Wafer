30519fd5c66e70397dc0df1946ad2199f82dce92
Tim DuBois <timothy.dubois@chalmers.se>
2017-05-04 12:53:30 +0200

    Read-in is now typed.
    Moved for now to json rather than hjson because the hjson plugin is
    still running on serde 0.8.x, so automatic type inference of the structs
    isn't happening.
    
    Upgrade to 1.0:
    https://github.com/hjson/hjson-rust/issues/3
    
    Deserialise test fails:
    https://github.com/sanderhahn/example-serde-hjson

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate serde;
-extern crate serde_hjson;
+extern crate serde_json;
 
-use serde_hjson::{Map,Value};
+#[macro_use]
+extern crate serde_derive;
+
 use std::error::Error;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
@@ -9,10 +11,65 @@ use std::path::Path;
 
 include!(concat!(env!("OUT_DIR"), "/version.rs"));
 
+#[derive(Serialize, Deserialize)]
+struct Config {
+    grid: Grid,
+    tolerance: f32,
+    max_steps: f64,
+    wavenum: u8,
+    wavemax: u8,
+    clustrun: bool,
+    al_clust: Point3,
+    output: Output,
+    potential: u8,
+    mass: f32,
+    init_condition: u8,
+    sig: f32,
+    init_symmetry: u8,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Grid {
+    x: u32,
+    y: u32,
+    z: u32,
+    dn: f32,
+    dt: f32,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Point3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Output {
+    screen_update: u32,
+    snap_update: u32,
+    save_wavefns: bool,
+    save_potential: bool,
+}
+
 fn read_file<P: AsRef<Path>>(file_path: P) -> Result<String, std::io::Error> {
     let mut contents = String::new();
     OpenOptions::new().read(true).open(file_path)?.read_to_string(&mut contents)?;
     Ok(contents)
+}
+
+fn load_config() -> Config {
+    //Read in configuration file (hjson format)
+    let raw_config = match read_file("wafer.cfg") {
+        Ok(s) => s,
+        Err(err) => panic!("Cannot read configuration file: {}.", err.description()),
+    };
+    // Decode configuration file.
+    let decoded_config: Config = match serde_json::from_str(&raw_config) {
+        Ok(c) => c,
+        Err(err) => panic!("Error parsing configuration file: {}.", err.description()),
+    };
+    decoded_config
 }
 
 
@@ -27,38 +84,9 @@ fn main() {
     println!("              \\__/");
     println!("");
 
-    //Read in configuration file (hjson format)
-    let raw_config = match read_file("wafer.cfg") {
-        Ok(s) => s,
-        Err(err) => panic!("Cannot read configuration file: {}.", err.description()),
-    };
+    let config = load_config();
 
-    println!("{:}", raw_config);
-    // Decode configuration file.
-    let config: Map<String, Value> = match serde_hjson::from_str(&raw_config) {
-        Ok(m) => m,
-        Err(err) => panic!("Cannot parse configuration file: {}.", err.description()),
-    };
-    println!("{:}", config["output"]);
-    println!("{:}", config["mass"]);
-
-    // scope to control lifetime of borrow
-    {
-        // Extract the rate
-        //let rate = sample.get("rate").unwrap().as_f64().unwrap();
-        //println!("rate: {}", rate);
-
-        // Extract the array
-        //let array : &mut Vec<Value> = sample.get_mut("array").unwrap().as_array_mut().unwrap();
-        //println!("first: {}", array.get(0).unwrap());
-
-        // Add a value
-        //array.push(Value::String("tak".to_string()));
-    }
-
-    // Encode to Hjson
-    //let sample2 = serde_hjson::to_string(&sample).unwrap();
-    //println!("Hjson:\n{}", sample2);
+    println!("{}, {:e}", config.al_clust.z, config.max_steps);
 
 }
 
