787e8778f61e61ad40740b13b8a6853059365f2e
Tim DuBois <timothy.dubois@chalmers.se>
2017-05-16 13:53:30 +0200

    Example readin file for Matlab. Completely bare.

---
+++ analysis/matlab/load_plain.m
@@ -0,0 +1,35 @@
+%% Wavefunction
+fid = fopen('output/wavefunction_0.dat');
+
+data = textscan(fid, '%u, %u, %u, %f');
+fclose(fid);
+
+dx = data{1};
+dy = data{2};
+dz = data{3};
+
+gs = reshape(data{4},max(dx)+1,max(dy)+1,max(dz)+1);
+
+clear data fid;
+
+mid = floor(max(dx)/2);
+pcolor(squeeze(abs(gs(:,mid,:)))); 
+shading flat
+
+%% Potential
+fid = fopen('output/potential.dat');
+
+data = textscan(fid, '%u, %u, %u, %f');
+fclose(fid);
+
+dx = data{1};
+dy = data{2};
+dz = data{3};
+
+potential = reshape(data{4},max(dx)+1,max(dy)+1,max(dz)+1);
+
+clear data fid;
+
+mid = floor(max(dx)/2);
+pcolor(squeeze(potential(:,mid,:))); 
+shading flat
