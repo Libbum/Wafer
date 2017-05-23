a5d52d21696b054c9df4573fa376fd968f70d7a4
Tim DuBois <timothy.dubois@chalmers.se>
2017-05-23 13:45:57 +0200

    Potential suite

--- src/potential.rs
+++ src/potential.rs
@@ -1,6 +1,7 @@
 use ndarray::{Array3, Zip};
 use ndarray_parallel::prelude::*;
 use std::error::Error;
+use std::f64::consts::PI;
 use std::fmt;
 
 use config::*;
@@ -85,12 +86,83 @@ fn potential(config: &Config, idx: &Index3) -> Result<f64, PotentialError> {
                 Ok(0.0)
             }
         }
-        PotentialType::ComplexCoulomb => {
+        PotentialType::Periodic => {
+            let mut temp = (2.*PI*(idx.x as f64-1.)/(num.x as f64-1.)).sin()*(2.*PI*(idx.x as f64-1.)/(num.x as f64-1.)).sin();
+            temp *= (2.*PI*(idx.y as f64-1.)/(num.y as f64-1.)).sin()*(2.*PI*(idx.y as f64-1.)/(num.y as f64-1.)).sin();
+            temp *= (2.*PI*(idx.z as f64-1.)/(num.z as f64-1.)).sin()*(2.*PI*(idx.z as f64-1.)/(num.z as f64-1.)).sin();
+            Ok(-temp+1.)
+        }
+        PotentialType::Coulomb => {
             let r = config.grid.dn * (calculate_r2(idx, &config.grid)).sqrt();
             if r < config.grid.dn {
                 Ok(-1. / config.grid.dn)
             } else {
                 Ok(-1. / r)
+            }
+        }
+        PotentialType::ComplexCoulomb => {
+            //TODO: This is real until we have Complex types
+            let r = config.grid.dn * (calculate_r2(idx, &config.grid)).sqrt();
+            if r < config.grid.dn {
+                Ok(-1. / config.grid.dn)
+            } else {
+                Ok(-1. / r)
+            }
+        }
+        PotentialType::ElipticalCoulomb => {
+            let dx = idx.x as f64 - (num.x as f64+1.)/2.;
+            let dy = idx.y as f64 - (num.y as f64+1.)/2.;
+            let dz = (idx.z as f64 - (num.z as f64+1.)/2.) * 2.;
+            let r = config.grid.dn * (dx*dx+dy*dy+dz*dz).sqrt();
+            if r < config.grid.dn {
+                Ok(0.0)
+            } else {
+                Ok(-1. / r + 1./config.grid.dn)
+            }
+        }
+        PotentialType::SimpleCornell => {
+            // NOTE: units here are GeV for energy/momentum and GeV^(-1) for distance
+            let r = config.grid.dn * (calculate_r2(idx, &config.grid)).sqrt();
+            if r < config.grid.dn {
+                Ok(4.*config.mass)
+            } else {
+                Ok(-0.5*(4./3.)/r + config.sig*r + 4.*config.mass)
+            }
+        }
+        PotentialType::FullCornell => {
+            //NOTE: units here are GeV for energy/momentum and GeV^(-1) for distance
+            let t = 1.0; //TODO: This should be an optional parameter for FullCornell only
+            let xi: f64 = 0.0; //TODO: This should be an optional parameter for FullCornell only
+            let dz = idx.z as f64 - (config.grid.size.z as f64+1.)/2.;
+            let r = config.grid.dn * (calculate_r2(idx, &config.grid)).sqrt();
+            let md = mu(t)*(1. + 0.07*xi.powf(0.2)*(1.-config.grid.dn*config.grid.dn*dz*dz/(r*r)))*(1.+xi).powf(-0.29);
+            if r < config.grid.dn {
+                Ok(4.*config.mass)
+            } else {
+                Ok(-alphas(2.*PI*t)*(4./3.)*(-md*r).exp()/r + config.sig*(1. - (-md*r).exp())/md  - 0.8*config.sig/(4.*config.mass*config.mass*r) + 4.*config.mass)
+            }
+        }
+        PotentialType::Harmonic => {
+            let r = config.grid.dn * (calculate_r2(idx, &config.grid)).sqrt();
+            Ok(r*r/2.)
+        }
+        PotentialType::ComplexHarmonic => {
+            //TODO: This is real until we have Complex types
+            let r = config.grid.dn * (calculate_r2(idx, &config.grid)).sqrt();
+            Ok(r*r/2.)
+        }
+        PotentialType::Dodecahedron => {
+            //Varför inte?
+            let dx = idx.x as f64 - (num.x as f64+1.)/2.;
+            let dy = idx.y as f64 - (num.y as f64+1.)/2.;
+            let dz = idx.z as f64 - (num.z as f64+1.)/2.;
+            let x = dx/((num.x as f64-1.)/2.);
+            let y = dy/((num.y as f64-1.)/2.);
+            let z = dz/((num.z as f64-1.)/2.);
+            if 12.70820393249937 + 11.210068307552588*x >= 14.674169922690343*z && 11.210068307552588*x <= 12.70820393249937 + 14.674169922690343*z && 5.605034153776295*(3.23606797749979*x - 1.2360679774997896*z) <= 6.*(4.23606797749979 + 5.23606797749979*y) && 18.1382715378281*x + 3.464101615137755*z <= 12.70820393249937 && 9.06913576891405*x + 15.70820393249937*y <= 12.70820393249937 + 3.464101615137755*z && 9.70820393249937*y <= 12.70820393249937 + 5.605034153776294*x + 14.674169922690343*z && 12.70820393249937 + 5.605034153776294*x + 9.70820393249937*y + 14.674169922690343*z >= 0. && 15.70820393249937*y + 3.464101615137755*z <= 12.70820393249937 + 9.06913576891405*x && 5.605034153776295*(-6.47213595499958*x - 1.2360679774997896*z) <= 25.41640786499874 && 3.464101615137755*z <= 9.06913576891405*x + 3.*(4.23606797749979 + 5.23606797749979*y) && 1.7320508075688772*(3.23606797749979*x + 8.47213595499958*z) <= 3.*(4.23606797749979 + 3.23606797749979*y) && 5.605034153776294*x + 9.70820393249937*y + 14.674169922690343*z <= 12.70820393249937 {
+                Ok(-100.)
+            } else {
+                Ok(0.0)
             }
         }
         PotentialType::FromFile |
@@ -103,14 +175,29 @@ fn potential(config: &Config, idx: &Index3) -> Result<f64, PotentialError> {
 //TODO: We need potential_sub file outputs for those which require it.
 // Then here from_file can be treated differently.
 pub fn potential_sub(config: &Config, idx: &Index3) -> Result<f64, PotentialError> {
-    //    let num = &config.grid.size;
-    let _idx = idx; //Supress index warning until we implement this.
+    //TODO: idx is only called for FullCornell, so we may want to match it outside of this call and
+    //use a special case. Then we can drop the index requirement completely here.
     match config.potential {
         PotentialType::NoPotential |
         PotentialType::Cube |
         PotentialType::QuadWell |
-        PotentialType::FromFile |
-        PotentialType::ComplexCoulomb => Ok(0.0),
+        PotentialType::Periodic |
+        PotentialType::Coulomb |
+        PotentialType::ComplexCoulomb |
+        PotentialType::Harmonic |
+        PotentialType::ComplexHarmonic |
+        PotentialType::Dodecahedron |
+        PotentialType::FromFile => Ok(0.0),
+        PotentialType::ElipticalCoulomb => Ok(1./config.grid.dn),
+        PotentialType::SimpleCornell => Ok(4.0*config.mass),
+        PotentialType::FullCornell => {
+            let dz = idx.z as f64 - (config.grid.size.z as f64+1.)/2.;
+            let r = config.grid.dn * (calculate_r2(idx, &config.grid)).sqrt();
+            let t = 1.0; //TODO: This should be an optional parameter for FullCornell only
+            let xi: f64 = 0.0; //TODO: This should be an optional parameter for FullCornell only
+            let md = mu(t)*(1. + 0.07*xi.powf(0.2)*(1.-config.grid.dn*config.grid.dn*dz*dz/(r*r)))*(1.+xi).powf(-0.29);
+            Ok(config.sig/md + 4.*config.mass)
+        }
         PotentialType::FromScript => Err(PotentialError::NotAvailable), //TODO: Script may not need to error.
     }
 }
@@ -120,4 +207,25 @@ pub fn calculate_r2(idx: &Index3, grid: &Grid) -> f64 {
     let dy = (idx.y as f64) - ((grid.size.y as f64) + 1.) / 2.;
     let dz = (idx.z as f64) - ((grid.size.z as f64) + 1.) / 2.; //TODO: DISTNUMZ (if needed)
     (dx * dx + dy * dy + dz * dz)
+}
+
+/// Running coupling
+fn alphas(mu: f64) -> f64 {
+    let nf = 2.0; //TODO: This should be an optional parameter for FullCornell only
+    let b0 = 11. - 2.*nf/3.;
+    let b1 = 51. - 19.*nf/3.;
+    let b2 = 2857. - 5033.*nf/9. + 325.*nf*nf/27.;
+
+    let r = 2.3; // scale adjusted to match lattice data from hep-lat/0503017v2
+
+    let l = 2.*(mu/r).ln();
+
+    4.*PI*(1. - 2.*b1*l.ln()/(b0*b0*l) + 4.*b1*b1*((l.ln()-0.5)*(l.ln()-0.5)+b2*b0/(8.*b1*b1)-5.0/4.0)/(b0*b0*b0*b0*l*l))/(b0*l)
+}
+
+/// Debye screening mass
+fn mu(t: f64) -> f64 {
+    let nf = 2.0; //TODO: This should be an optional parameter for FullCornell only
+    let tc = 0.2; //TODO: This should be an optional parameter for FullCornell only
+    1.4*((1.+nf/6.)*4.*PI*alphas(2.*PI*t)).sqrt()*t*tc
 }
